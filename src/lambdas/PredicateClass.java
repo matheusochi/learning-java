62060ea16201b726342686fb1bd9a8c995dfe16d
Matheus Ochi <mths.ochi@gmail.com>
2023-07-15 14:47:40 -0300

    feat(lambdas): predicate implementation

---
+++ src/lambdas/PredicateClass.java
@@ -0,0 +1,15 @@
+package lambdas;
+
+import java.util.function.Predicate;
+
+public class PredicateClass {
+    public static void main(String[] args) {
+        Predicate<Product> hasDiscount = (product -> product.discount > 0);
+
+        Product product = new Product("Skate", 2400, 0);
+        System.out.println(hasDiscount.test(product));
+
+        Product product1 = new Product("Notebook", 5789,2);
+        System.out.println(hasDiscount.test(product1));
+    }
+}
