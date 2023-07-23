fa0d3ba855bd933739c0dd00d56f289cfc67cfad
Matheus Ochi <mths.ochi@gmail.com>
2023-07-23 18:16:44 -0300

    feat(stream): reduce implementation using non primitive values

---
+++ src/streams/Reduce3.java
@@ -0,0 +1,34 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Reduce3 {
+    public static void main(String[] args) {
+        Student s1 = new Student("Ana", 7.1);
+        Student s2 = new Student("Gabi", 6.1);
+        Student s3 = new Student("Gui", 8.1);
+        Student s4 = new Student("Rafa", 10);
+
+        List<Student> students = Arrays.asList(s1, s2, s3, s4);
+
+        Predicate<Student> approved = student -> student.grade >= 7;
+        Function<Student, Double> getGrade = student -> student.grade;
+
+        BiFunction<Average, Double, Average> calculateAverage = (average, grade) -> {
+            return average.add(grade);
+        };
+
+        var a = students.stream()
+                .filter(approved)
+                .map(getGrade)
+                .reduce(new Average(), calculateAverage, Average::combine)
+                .getValue();
+
+        System.out.println(a);
+    }
+}
